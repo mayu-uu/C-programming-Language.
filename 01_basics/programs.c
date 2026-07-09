756e7f7cd72d2dbb0ce99d02d2c705db16c419c7
mayu-uu <mayukh0308@gmail.com>
2026-07-09 23:54:17 +0530

    Update programs.c

--- 01_basics/programs.c
+++ 01_basics/programs.c
@@ -1,1 +1,6 @@
+#include <stdio.h> //stdio header file has information about standard I/O.
 
+int main(){
+  printf("Hello , Mayukh here!");
+  return 0; //incase of stdlib header is included , exit(0) can also be used.
+}
