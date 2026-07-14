f82ebb27d997aec57e0118c1de1dc53802cc6308
mayu-uu <mayukh0308@gmail.com>
2026-07-14 22:56:57 +0530

    Delete 01_basics directory

--- 01_basics/programs.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h> //stdio header file has information about standard I/O.
-
-int main(){
-  printf("Hello , Mayukh here!");
-  return 0; //incase of stdlib header is included , exit(0) can also be used.
-}
