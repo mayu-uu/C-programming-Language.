33295ea123558a5f5d3cde231cabe133349b4c52
crofesim <crofesima06@gmail.com>
2026-07-15 15:44:35 +0530

    codes

---
+++ method01code.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int a=2;
+    int b=3;
+    printf("Initial numbers were: %d %d\n", a, b);
+    int s=a;
+    a=b;
+    b=s;
+    printf("swapped numbers are: %d %d", a, b);
+    return 0;
+}
